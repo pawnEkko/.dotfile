ed777e8722891066a3f766818c52ecf5602eec39
pawnEkko <loveangelforver26@gmail.com>
2023-07-16 13:41:03 +0800

    增加和修改

--- nvim/.config/nvim/lua/plugins/UI.lua
+++ nvim/.config/nvim/lua/plugins/UI.lua
@@ -1,41 +1,66 @@
 return {
+	-----------------------花活UI注释保护----------------------
+
 	-- notify
-	{
-		"rcarriga/nvim-notify",
-		opts = {
-			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-		},
-		init = function()
-			local Util = require("util")
-			if not Util.has("noice.nvim") then
-				Util.on_very_lazy(function()
-					vim.notify = require("notify")
-				end)
-			end
-		end,
-	},
-	-- dressing
-	{
-		"stevearc/dressing.nvim",
-		lazy = true,
-		init = function()
-			vim.ui.select = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.select(...)
-			end
-			vim.ui.input = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.input(...)
-			end
-		end,
-	},
-	-- bufferline
+	--
+	-- {
+	-- 	"rcarriga/nvim-notify",
+	-- 	opts = {
+	-- 		timeout = 3000,
+	-- 		max_height = function()
+	-- 			return math.floor(vim.o.lines * 0.75)
+	-- 		end,
+	-- 		max_width = function()
+	-- 			return math.floor(vim.o.columns * 0.75)
+	-- 		end,
+	-- 	},
+	-- 	init = function()
+	-- 		local Util = require("util")
+	-- 		if not Util.has("noice.nvim") then
+	-- 			Util.on_very_lazy(function()
+	-- 				vim.notify = require("notify")
+	-- 			end)
+	-- 		end
+	-- 	end,
+	-- },
+
+	-- noice
+	--
+	-- {
+	-- 	"folke/noice.nvim",
+	-- 	event = "VeryLazy",
+	-- 	opts = {
+	-- 		lsp = {
+	-- 			override = {
+	-- 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+	-- 				["vim.lsp.util.stylize_markdown"] = true,
+	-- 				["cmp.entry.get_documentation"] = true,
+	-- 			},
+	-- 		},
+	-- 		routes = {
+	-- 			{
+	-- 				filter = {
+	-- 					event = "msg_show",
+	-- 					any = {
+	-- 						{ find = "%d+L, %d+B" },
+	-- 						{ find = "; after #%d+" },
+	-- 						{ find = "; before #%d+" },
+	-- 					},
+	-- 				},
+	-- 				view = "mini",
+	-- 			},
+	-- 		},
+	-- 		presets = {
+	-- 			bottom_search = true,
+	-- 			command_palette = true,
+	-- 			long_message_to_split = true,
+	-- 			inc_rename = true,
+	-- 		},
+	-- 	},
+	-- },
+
+	------------------------花活UI注释保护结束---------------------
+
 	{
 		"akinsho/bufferline.nvim",
 		event = "VeryLazy",
@@ -120,11 +145,6 @@ return {
 							color = Util.fg("Debug"),
 						},
 						{
-							require("lazy.status").updates,
-							cond = require("lazy.status").has_updates,
-							color = Util.fg("Special"),
-						},
-						{
 							"diff",
 							symbols = {
 								added = icons.git.added,
@@ -167,39 +187,6 @@ return {
 			},
 			show_trailing_blankline_indent = false,
 			show_current_context = false,
-		},
-	},
-	-- noice
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "%d+L, %d+B" },
-							{ find = "; after #%d+" },
-							{ find = "; before #%d+" },
-						},
-					},
-					view = "mini",
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = true,
-			},
 		},
 	},
 	{
